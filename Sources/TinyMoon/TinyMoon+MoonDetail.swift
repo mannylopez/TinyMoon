bf228669b528bc0658dd29512b81b92b8287f2f8
mannylopez <manuel.est.lopez@gmail.com>
2024-07-13 10:33:36 -0700

    Add MoonDetail to public API

--- Sources/TinyMoon/TinyMoon+MoonDetail.swift
+++ Sources/TinyMoon/TinyMoon+MoonDetail.swift
@@ -6,16 +6,17 @@ import Foundation
 
 extension TinyMoon {
 
-  struct MoonDetail {
-    let julianDay: Double
+  public struct MoonDetail: Hashable {
+
+    public let julianDay: Double
     /// Number of days elapsed into the synodic cycle, represented as a fraction
-    let daysElapsedInCycle: Double
+    public let daysElapsedInCycle: Double
     /// Age of the moon in days, minutes, hours
-    let ageOfMoon: (days: Int, hours: Int, minutes: Int)
+    public let ageOfMoon: (days: Int, hours: Int, minutes: Int)
     /// Illuminated portion of the Moon, where 0.0 = new and 0.99 = full
-    let illuminatedFraction: Double
+    public let illuminatedFraction: Double
     /// Distance of moon from the center of the Earth, in kilometers
-    let distanceFromCenterOfEarth: Double
+    public let distanceFromCenterOfEarth: Double
     /// Phase of the Moon, represented as a fraction
     ///
     /// Varies between `0.0` to `0.99`.
@@ -23,6 +24,29 @@ extension TinyMoon {
     /// `0.25` first quarter,
     /// `0.5` full moon,
     /// `0.75` last quarter
-    let phase: Double
+    public let phase: Double
+
+    public static func == (lhs: TinyMoon.MoonDetail, rhs: TinyMoon.MoonDetail) -> Bool {
+      lhs.julianDay == rhs.julianDay &&
+        lhs.daysElapsedInCycle == rhs.daysElapsedInCycle &&
+        lhs.ageOfMoon.days == rhs.ageOfMoon.days &&
+        lhs.ageOfMoon.hours == rhs.ageOfMoon.hours &&
+        lhs.ageOfMoon.minutes == rhs.ageOfMoon.minutes &&
+        lhs.illuminatedFraction == rhs.illuminatedFraction &&
+        lhs.distanceFromCenterOfEarth == rhs.distanceFromCenterOfEarth &&
+        lhs.phase == rhs.phase
+    }
+
+    public func hash(into hasher: inout Hasher) {
+      hasher.combine(julianDay)
+      hasher.combine(daysElapsedInCycle)
+      hasher.combine(ageOfMoon.days)
+      hasher.combine(ageOfMoon.hours)
+      hasher.combine(ageOfMoon.minutes)
+      hasher.combine(illuminatedFraction)
+      hasher.combine(distanceFromCenterOfEarth)
+      hasher.combine(phase)
+    }
+
   }
 }
